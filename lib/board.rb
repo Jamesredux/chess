54dcb9b835b655a63550a0f6aac69a5f7109e69a
James McCann <jamesredux@gmail.com>
2017-12-28 19:02:56 +0700

    create board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,32 @@
 #board will go here
+
+class Board 
+
+	def initialize
+		@board = create_board
+
+	end
+
+	def create_board
+		board = Array.new(64, ' ')
+		board
+
+	end	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end	
