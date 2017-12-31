2f2b38ed87b7f227f7170fd41f2b8f671136d47b
James McCann <jamesredux@gmail.com>
2017-12-31 11:52:58 +0700

    create Board and Cell class

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,9 @@
 #board will go here
+require_relative 'cell'
 
-class Board 
+class BoardClass
+
+	attr_accessor :board
 
 	def initialize
 		@board = create_board
@@ -8,25 +11,31 @@ class Board
 	end
 
 	def create_board
-		board = Array.new(64, ' ')
-		board
+		board = []
+		64.times do |x|
+			x = Cell.new
+			board<<x 
+		end
+		board 
 
 	end	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	def draw_board
+		a = @board[0..7]
+		b = @board[8..15]
+		puts a.inspect
+		bottom_line = []
+		a.each do |x| 
+			bottom_line << x.contents
+		end
+		puts bottom_line.inspect
+	end
 
 
 
 end	
+
+x = BoardClass.new
+x.draw_board
+x.board[7].contents = 'pawn'
+x.draw_board
---
+++ lib/cell.rb
@@ -0,0 +1,15 @@
+
+
+class Cell 
+
+	attr_accessor :contents, :color
+
+	def initialize
+		@contents = 0
+		@color = false
+	end	
+
+
+
+end	
+
