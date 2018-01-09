fe8a76c5ce02ea06d471d1dc81ec0e7fc5f61a3f
James McCann <jamesredux@gmail.com>
2018-01-09 20:55:53 +0700

    work on move check in test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -1,79 +1,49 @@
 
-#cell_1, cell_8 = "\u265C","\u265C"
-#cell_2, cell_7 = "\u265E","\u265E"
-#cell_3, cell_6 = "\u265D","\u265D"
-#cell_4 = "\u265B"
-#cell_5 = "\u265A"
-
-#array_0 = ["\u265C", "\u265E", "\u265D", "\u265B", "\u265A", "\u265D", "\u265E", "\u265C"]
-
-
-#row = [cell_1, cell_2, cell_3, cell_4, cell_5, cell_6, cell_7, cell_8]
 
 
 
-row = ["\u265C", "\u265E", "\u265D", "\u265B", "\u265A", "\u265D", "\u265E", "\u265C"]
+#this method takes the cell the piece is moving from and the piece the cell is moving to and 
+# gives the difference in coordinates -- may be useful
+# if you want to move something you just take the - in reduce and change it to a +
+#but using a mathimatical thing like this may have problems a white piece going backwards will have the same move 
+#equation as a black piece going forwards
 
 
-def draw_row
+#this method works i think?
+def move_check(move_array)
+	@valid = false
+	white_rook_moves_up = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0]]
+	white_rook_moves_down = [[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0]]
+	white_rook_moves_across_r = [[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7]]
+	white_rook_moves_across_l = [[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7]]
+	white_rook_move_set = [white_rook_moves_up, white_rook_moves_down, white_rook_moves_across_r, white_rook_moves_across_l]
+
 	
-	#row = [1, 2, cell3, cell_4, cell_5, cell_6, cell_7, cell_8]
-	array_index = 0
-	y = 1
-	row = []
-	(1..8).each do |x|
-		"cell_" + x.to_s = array_0[array_index]
-		row << var
-		array_index += 1
-		y += 1
-	end
+
+
+	white_rook_move_set.each do |x|
+		x.each do |y|
+			if move_array == y 
+				@valid = true
+			end
+		end
+	end			
+	@valid
 end
 
 
 
-puts "\u2551#{row[0]} \u2551#{row[1]} \u2551#{row[2]} \u2551#{row[3]} "\
-		 "\u2551#{row[4]} \u2551#{row[5]} \u2551#{row[6]} \u2551#{row[7]} \u2551"
+def move(old_cell, new_cell)
+	
+		move_array = [new_cell, old_cell].transpose.map { |y| y.reduce(:-)} 
+		 move_check(move_array)
+end	
 
 
-def draw_board
-	@board.each do |x|
-		create_row(x)
-	end
 
-def create_row(array)
-	row = []
-	array.each do |x|
-		row<< x.symbol
-	end
-		puts "\u2551#{row[0]} \u2551#{row[1]} \u2551#{row[2]} \u2551#{row[3]} "\
-		 "\u2551#{row[4]} \u2551#{row[5]} \u2551#{row[6]} \u2551#{row[7]} \u2551"
-end	 	
-					 
+	
 
-pieces_index = { -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A",
-									-1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A", -1 =>	"\u265A"}
-		when -2 #black queen
-			"\u265B"
-		when -3 #black rook
-			"\u265C"
-		when -4 #black bishop
-			"\u265D"
-		when -5 #black knight
-			"\u265E"
-		when -6 #black pawn
-			"\u265F"				
-
-		when 1 #white king
-			"\u2654"
-		when 2 #white queen
-			"\u2655"
-		when 3 #white rook
-			"\u2656"
-		when 4 #white bishop
-			"\u2657"
-		when 5 #white knight
-			"\u2658"
-		when 6 #white pawn
-			"\u2659"
-		end
-	end
+puts move([7,7], [0,7])
+puts move([6,3],[5,4])
+puts move([7,1], [5,2])
+puts move([7,0],[8,0])
